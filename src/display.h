832535328731d0490f4d75b96a6d6d62d8ccb600
Thea Go <theaellengo@gmail.com>
2021-04-13 10:08:54 +0800

    remove code

--- src/display.h
+++ src/display.h
@@ -11,22 +11,6 @@ void printgnatt(Process process[], int n);
 void printlabel(char* str);
 int getnumdigits(int num);
 
-void ptemp(Process process[], int n)
-{
-  printlabel("\n\nProcess\tArrival\tArrtime\tBurst\tExec\tStart\tEnd\tWaiting\tTurnaround\n");
-  for (int i = 0; i < n; i++) {
-    printf("%d\t", process[i].pid);
-    printf("%d\t", process[i].arrival);
-    printf("%d\t", process[i].arrtime);
-    printf("%d\t", process[i].burst);
-    printf("%d\t", process[i].exectime);
-    printf("%d\t", process[i].start);
-    printf("%d\t", process[i].completion);
-    printf("%d\t", process[i].waiting);
-    printf("%d\n", process[i].turnaround);
-  }
-}
-
 void printprocess(Process process[], int n, float awt)
 {
   printlabel("\n\nProcess   Arrival   Burst   Start   End   Waiting   Turnaround\n");
