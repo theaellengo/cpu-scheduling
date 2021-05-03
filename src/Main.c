7ffab215ff51f0d6b498775cecd354dd8ec29c07
Thea Go <59685009+theaellengo@users.noreply.github.com>
2021-05-03 11:08:49 +0800

    Update and rename CSOPESY-Project1-S15-Group22.c to Main.c

--- src/Main.c
+++ src/Main.c
@@ -1,10 +1,3 @@
-/***************************************************************
-Name: Go, Thea Ellen
-      Chong, Dianne
-Group: 22
-Section: S12
-***************************************************************/
-
 // include libraries
 #include "fcfs.h"
 #include "nsjf.h"
